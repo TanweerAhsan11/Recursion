8bde234c6006afbd8f037e74546fd8338e18b659
TanweerAhsan11 <hasantanveer624@gmail.com>
2023-08-08 19:58:58 +0530

    Count the number of subset sum equal to given sum.

---
+++ sumSubsetProblem.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class sumSubsetProblem {
+    public static int countSubset(int arr[],int n,int sum)
+    {
+        if(n==0)
+        {
+            return (sum==0) ? 1:0;
+        }
+        return countSubset(arr,n-1,sum) + countSubset(arr,n-1,sum-arr[n-1]);
+    }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int sum=s.nextInt();
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        int c=countSubset(arr,n,sum);
+        System.out.println(c);
+    }
+}
