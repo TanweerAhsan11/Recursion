2fb14f667d49f54ac875e2a47b7340199a180648
TanweerAhsan11 <hasantanveer624@gmail.com>
2023-08-08 19:07:14 +0530

    TOH Recursive

---
+++ TowerOfHanoi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class TowerOfHanoi {
+    public static void TOH(int n,char a,char b,char c)
+    {
+        if(n==1)
+        {
+            System.out.println("Move 1 from " + a + " to " + c);
+            return;
+        }
+        TOH(n-1,a,c,b);
+        System.out.println("Move " + n + " from " + a + " to " + c );
+        TOH(n-1,b,a,c);
+    }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        char a='A';
+        char b='B';
+        char c='C';
+        TOH(n,a,b,c);
+
+    }
+}
